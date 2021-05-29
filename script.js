b33e44f13defc8c04fe8ac9f759fd63b50e587cc
unknown <lokobanti90@gmail.com>
2021-05-29 13:55:12 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// import { request, gql } from 'graphql-request'
+
+// const query = gql`
+//   {
+//     Movie(title: "Inception") {
+//       releaseDate
+//       actors {
+//         name
+//       }
+//     }
+//   }
+// `
+
+// request('https://api.graph.cool/simple/v1/movies', query).then((data) => console.log(data))
+
+// fetch('https://docs.github.com/en/graphql/overview/explorer/')
+//   .then(response => response.json())
+//   .then(data => console.log(data))
